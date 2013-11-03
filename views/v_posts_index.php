cc5d3fed28757c5cf3f973318793615654bcc10f
Jackson Yeh <jcyeh5@hotmail.com>
2013-11-03 17:44:52 -0500

    CSS for posts

--- views/v_posts_index.php
+++ views/v_posts_index.php
@@ -1,22 +1,22 @@
 <?php foreach($posts as $post): ?>
 
 <div class = "post">
-	<article>
-		
-		<h1><?=$post['first_name']?> <?=$post['last_name']?> posted:</h1>
-
-		<p><?=$post['content']?></p>
-		
-		<time datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
+	<article>		
+		<p id="post_author"><?=$post['first_name']?> <?=$post['last_name']?></p>
+		<time id="post_time" datetime="<?=Time::display($post['created'],'Y-m-d G:i')?>">
 			<?=Time::display($post['created'])?>
 		</time>
+		
+		<p id="post_content"><?=$post['content']?></p>
+		
+
 
 		<!-- Display Delete button only for own posts -->
 	    <?php if($user->user_id == $post['post_user_id']): ?>
 			<div align= right> <a href='/posts/delete/<?=$post['post_id']?>'><img src="/images/delete.png"></a> </div>
 		<?php endif; ?>	
 
-		</article>
+	</article>
 </div>
 
 <?php endforeach; ?>
