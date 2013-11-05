10e507cfed70065d0baca56d757ec9547fd4e2a9
Jackson Yeh <jcyeh5@hotmail.com>
2013-11-05 02:39:08 -0500

    CSS

--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,4 +1,5 @@
 <div id="loginframe">
+
 	<!-- Login Form -->
 	<form id= "loginform" method='POST' action='/users/p_login'>
 
@@ -18,15 +19,28 @@
 		<?php endif; ?>
 				
 		<input type='submit' value='Log in'>
+		<br><br><br><br>
+	
 	</form>
+
 
 	<!-- Verbiage for screen -->
 	<div id="welcometextdiv">
-		<p>Welcome to My 2 Cents.  If you have an account already, please sign in.  If you do not have an account, 
-		please sign up to start giving the rest of the world a piece of your mind.</p>
+
+		<h2>+1 features on this site</h2>
+		<ul>
+			<li>Delete a post</li>
+			<li>Display and edit profile</li>
+			<li>Reset password</li>
+			<li>"LIKE" feature</li>
+			<li>User statistics (i.e. number of followers, number of posts, etc.)</li>
+		</ul>
+			
 	
 		<!-- Signup button -->	
 		<div id="signupbuttondiv"><a href='/users/signup'><img  src="/images/signuptoday.png" alt="link to sign up form"></a></div>
+		
+		
 	</div>
 	
 	
